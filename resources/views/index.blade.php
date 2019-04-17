9ae353c258175e758107ba2b9f17f19d6031babe
bennyekop <bennyekop4@gmail.com>
2019-04-17 13:18:24 +0700

    deploy laravel docker

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('base')
+@section('content')
+    <!-- Main Section -->
+    <section class="main-section">
+        <!-- Add Your Content Inside -->
+        <div class="content">
+            <!-- Remove This Before You Start -->
+            <h1>Rumahweb -  Tutorial Laravel </h1>
+            <p>Apa saja yang akan kamu pelajari ?</p>
+
+<<<<<<< HEAD
+            <h2>* Templating Laravel </h2>
+            <h2>- Basic Web Service dengan Laravel </h2>
+=======
+            <h2>* Kirim E-mail dengan Laravel  /email </h2>
+>>>>>>> 0cd2a0203b56511838deb622e446bcfd1db66eab
+        </div>
+        <!-- /.content -->
+    </section>
+    <!-- /.main-section -->
+@endsection
